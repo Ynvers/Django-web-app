985bd2828a49f2318abdeecd76cccd13d16b5272
Ynvers <luzolo.adoho@gmail.com>
2024-07-20 14:41:34 +0100

    Fin du premier chapitre

---
+++ merchex/listings/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Band(models.Model):
+    name = models.fields.CharField(max_length=100)
+
+class Listing(models.Model): 
+    title = models.fields.CharField(max_length=100)
---
+++ merchex/listings/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from listings.models import Band, Listing
+
+def hello(request):
+    bands = Band.objects.all()
+    return render(request, 'listings/hello.html',
+                    {'bands' : bands})
+
+def about(request):
+    return render(request, 'listings/about.html')
+
+def listings(request):
+    listings = Listing.objects.all()
+    return render(request, 'listings/listings.html', 
+                    {'listings' : listings})
+    
+
+def contact(request):
+
+    return render(request, 'listings/contact.html')
